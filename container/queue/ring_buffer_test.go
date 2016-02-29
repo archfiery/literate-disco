6df70ed791824bab976132fd610d76179281ac0b
John <archfiery@gmail.com>
2016-02-29 17:39:02 +1100

    implement mpmc ring buffer

---
+++ container/queue/ring_buffer_test.go
@@ -0,0 +1,95 @@
+package queue
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"testing"
+)
+
+func TestRingBufferEnqueue(t *testing.T) {
+	fmt.Println("\nTestRingBufferEnqueue")
+	rb, err := MakeRingBuffer(6)
+	if err != nil || len(rb.buffer_) != 8 {
+		t.Fatal("The ring buffer size is not correctly set")
+	}
+
+	succ := rb.Enqueue(1)
+	if succ == false {
+		t.Fatal("Fail to enqueue")
+	}
+
+	if rb.Size() != 1 {
+		t.Fatal("Size is incorrect")
+	}
+
+	val, succ := rb.Dequeue()
+	if succ == false {
+		t.Fatal("Fail to dequeue")
+	}
+	if val != 1 {
+		t.Fatal("Value is incorrect")
+	}
+
+	if rb.Size() != 0 {
+		t.Fatal("Size is incorrect")
+	}
+}
+
+func TestRingBufferEnqueueDequeue(t *testing.T) {
+	fmt.Println("TestRingBufferEnqueueDequeue")
+	rb, err := MakeRingBuffer(9)
+	if err != nil || len(rb.buffer_) != 16 {
+		t.Fatal("The ring buffer size is not correctly set")
+	}
+
+	rb.Enqueue(1)
+	rb.Enqueue(2)
+	val, _ := rb.Dequeue()
+	if val != 1 {
+		t.Fatal("Incorrect dequeued value, expected 1, but it is ", val)
+	}
+	rb.Enqueue(15)
+	val, _ = rb.Dequeue()
+	if val != 2 {
+		t.Fatal("Incorrect dequeued value, expected 2, but it is ", val)
+	}
+	val, _ = rb.Dequeue()
+	if val != 15 {
+		t.Fatal("Incorrect dequeued value, expected 15, but it is ", val)
+	}
+	rb.Enqueue(12)
+	if rb.Size() != 1 || rb.Cap() != 16 {
+		t.Fatal("Expected size is 1, but it is  ", rb.Size(), ". Expected cap is ", rb.Cap())
+	}
+}
+
+//==============
+// Benchmark
+//==============
+
+func BenchmarkSinglePRRingBuffer(b *testing.B) {
+	rb, _ := MakeRingBuffer(1024)
+	var p float64
+	p = 20
+	var i uint64
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i = 0; i < uint64(math.Exp2(p)); i++ {
+			rb.Enqueue(i)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i = 0; i < uint64(math.Exp2(p)); i++ {
+			rb.Dequeue()
+		}
+	}()
+
+	wg.Wait()
+}
